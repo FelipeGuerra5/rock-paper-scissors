cadf0661664722bc2c40a7697a0a8d6f0f34186a
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-03-27 01:30:20 -0300

    Updating StyleSheet

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const container = document.getElementById('container')
 // Structure build
 function makeElement(dic) { // Accepts => id, text, classes=[], children=[], parent
     const element = document.createElement(dic['tag'])
-    dic['id'] ? element.id = dic['id'] : "" 
+    dic['id'] ? element.id = dic['id'] : ""
     dic['text'] ? element.innerHTML = dic['text'] : ""
     dic['classes'] ? dic['classes'].forEach((item) => element.classList.add(item)) : ""
     dic['children'] ? dic['children'].forEach((item) => element.appendChild(item)) : ""
@@ -13,103 +13,103 @@ function makeElement(dic) { // Accepts => id, text, classes=[], children=[], par
 
 const title = makeElement(
     {
-        'tag':'h1',
-        'text':'Rock, Paper, Scissors',
-        'parent':container
+        'tag': 'h1',
+        'text': 'Rock - Paper - Scissors',
+        'parent': container
     }
 )
- 
+
 const display = makeElement(
     {
-        'tag':'div',
-        'classes':['diplay'],
-        'parent':container
+        'tag': 'div',
+        'classes': ['display'],
+        'parent': container
     }
 )
 
 const displayButtons = makeElement(
     {
-        'tag':'div',
-        'classes':['choices'],
-        'parent':container
+        'tag': 'div',
+        'classes': ['choices'],
+        'parent': container
     }
 )
 
 const pcChoiceSpan = makeElement(
     {
-        'tag':'span',
-        'classes':['computer-choice']
+        'tag': 'span',
+        'classes': ['computer-choice']
     }
 )
 
 const pcChoice = makeElement(
     {
-        'tag':'h2',
+        'tag': 'h2',
         'text': "Computer's Choice: ",
-        'children':[pcChoiceSpan],
-        'parent':display
+        'children': [pcChoiceSpan],
+        'parent': display
     }
 )
 
 const userChoiceSpan = makeElement(
     {
-        'tag':'span',
-        'classes':['user-choice']
+        'tag': 'span',
+        'classes': ['user-choice']
     }
 )
-        
+
 const userChoice = makeElement(
     {
-        'tag':'h2',
-        'text':"User's Choice: ",
-        'children':[userChoiceSpan],
-        'parent':display
+        'tag': 'h2',
+        'text': "User's Choice: ",
+        'children': [userChoiceSpan],
+        'parent': display
     }
 )
-            
+
 const resultSpan = makeElement(
     {
-        'tag':'span',
-        'classes':['result']
+        'tag': 'span',
+        'classes': ['result']
     }
 )
 
 const result = makeElement(
     {
-        'tag':'h2',
+        'tag': 'h2',
         'text': "Result: ",
-        'children':[resultSpan],
-        'parent':display
+        'children': [resultSpan],
+        'parent': display
     }
 )
 
 const buttonRock = makeElement(
     {
-        'tag':'button',
-        'classes':['button'],
-        'id':'rock',
-        'text':'Rock',
-        'parent':displayButtons
+        'tag': 'button',
+        'classes': ['button'],
+        'id': 'Rock',
+        'text': 'Rock',
+        'parent': displayButtons
     }
 )
 
 const buttonPaper = makeElement(
     {
-        'tag':'button',
-        'classes':['button'],
-        'id':'paper',
-        'text':'Paper',
-        'parent':displayButtons
+        'tag': 'button',
+        'classes': ['button'],
+        'id': 'Paper',
+        'text': 'Paper',
+        'parent': displayButtons
     }
 )
 
 const buttonScissors = makeElement(
     {
-        'tag':'button',
-        'classes':['button'],
-        'id':'scissors',
-        'text':'Scissors',
-        'parent':displayButtons
+        'tag': 'button',
+        'classes': ['button'],
+        'id': 'Scissors',
+        'text': 'Scissors',
+        'parent': displayButtons
     }
 )
 
@@ -120,21 +120,20 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
     userChoiceSpan.innerHTML = userChoice
     let computerChoice = generateComputerChoice()
     pcChoiceSpan.innerHTML = computerChoice
-    console.log(userChoice, computerChoice)
     resultSpan.innerHTML = getResult(userChoice, computerChoice)
 }))
 
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * possibleChoices.length)
-    const arr = ['rock', 'paper', 'scissors']
+    const arr = ['Rock', 'Paper', 'Scissors']
     return arr[randomNumber]
 }
 
-function getResult(u , c) {
+function getResult(u, c) {
     if (u === c) return 'Draw!'
-    if ((u === 'rock' && c === "scissors") || 
-        (u === 'paper' && c === "rock") ||
-        (u === 'scissors' && c === "paper") 
-        ) return 'You Win!'
-        return 'You Lost!'
+    if ((u === 'Rock' && c === "Scissors") ||
+        (u === 'Paper' && c === "Rock") ||
+        (u === 'Scissors' && c === "Paper")
+    ) return 'You Win!'
+    return 'You Lost!'
 }
